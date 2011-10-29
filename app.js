8987e7dfd0514e30d182048b02fcd1454b2d4bfb
Yoshihide Chubachi <yoshi@chubachi.net>
2011-10-29 14:20:22 +0900

    changed port #, added code for session

--- app.js
+++ app.js
@@ -4,13 +4,19 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
+  , routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
+  // see: http://stackoverflow.com/questions/5599083/how-to-use-sessions-in-express-couchdb-and-node-js
+  var MemoryStore = require('connect').session.MemoryStore;
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: "keyboard cat", store: new MemoryStore({ reapInterval:  60000 * 10 })}));
+
+  // default conifgure
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
@@ -31,5 +37,5 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(1234);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
